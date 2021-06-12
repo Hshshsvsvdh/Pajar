07ab6453863c3d39d1c285991ffd7764dca75f5c
versijahat <85748558+versijahat@users.noreply.github.com>
2021-06-12 22:07:06 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvzf nheqminer-Linux-v0.8.2.tgz
-tar xf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l na.luckpool.net:3956 -u RWj54LVub5JZiavPMr9DayJZNQ1cheae63.Pajar -p x -t 8
